211a12b6705a46f175ac5c7de1df5971dd98b3a4
Chad Middleton <chad.middleton@tokenring.com>
2023-11-16 14:29:07 -0600

    init commit

---
+++ src/main/java/com/chadmiddleton/dynastyx/DynastyXApplication.java
@@ -0,0 +1,13 @@
+package com.chadmiddleton.dynastyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynastyXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynastyXApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chadmiddleton/dynastyx/DynastyXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chadmiddleton.dynastyx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynastyXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
